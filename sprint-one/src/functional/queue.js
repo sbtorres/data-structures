d8686516cf458f9484e041cf8672492457f3c6bc
rafeautie <rautie12@gmail.com>
2019-04-10 12:28:50 -0700

    Completed queue using functional style.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -5,14 +5,37 @@ var Queue = function() {
   var storage = {};
 
   // Implement the methods below
-
+  // New item in the queue added the end.
   someInstance.enqueue = function(value) {
+    var keys = Object.keys(storage);
+    if (value === undefined) {
+      return;
+    }
+
+    if (keys.length === 0){
+      storage['1'] = value;
+    } else {
+      storage[keys.length + 1] = value;
+    }
+    return storage;
   };
 
+  // Remove the next up item.
   someInstance.dequeue = function() {
+    var keys = Object.keys(storage);
+    var dequeued = storage[keys[0]];
+    delete storage[keys[0]];
+    for(var key in storage){
+      var newKey = Number(key) - 1;
+      storage[newKey] = storage[key];
+    }
+    delete storage[keys.length];
+    return storage;
   };
 
+  // Return size of queue
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
