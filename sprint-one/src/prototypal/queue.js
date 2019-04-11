b4fe6a7c597066f70562ae3cf291b4c42f7dbe7e
rafeautie <rautie12@gmail.com>
2019-04-10 20:50:27 -0700

    Completed queue.js in prototypal

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,22 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var obj = Object.create(queueMethods);
+  obj.storage = {};
+  return obj;
 };
-
-var queueMethods = {};
-
-
+var queueMethods = {
+  enqueue: function(value){
+    this.storage[this.size() + 1] = value;
+  },
+  dequeue: function(){
+    var dequeued = this.storage['1'];
+    delete this.storage['1'];
+    for(var key in this.storage){
+      this.storage[Number(key) - 1] = this.storage[key];
+    }
+    delete this.storage[this.size()];
+    return dequeued;
+  },
+  size: function(){
+    return Object.keys(this.storage).length;
+  }
+};
