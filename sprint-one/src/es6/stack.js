4e3697d076dfdfb2ed745d74f02d7fa814a5cd60
rafeautie <rautie12@gmail.com>
2019-04-10 22:16:38 -0700

    Completed stack.js using ES6 syntax.

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,22 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+  }
+
+  push(value) {
+    this.storage[this.size() + 1] = value;
+  }
+
+  pop() {
+    var popped = this.storage[this.size()];
+    delete this.storage[this.size()];
+    return popped;
+
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
   }
 
 }
