056a315fdb676736246760653cc2d67c270e2075
rafeautie <rautie12@gmail.com>
2019-04-10 18:13:48 -0700

    Refactored to not use Object.keys outside of size().

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -2,12 +2,12 @@ var extend = function(dest, targ){
   for(key in targ){
     dest[key] = targ[key];
   }
-}
+};
 
 var Queue = function() {
   var someInstance = {};
 
-  someInstance .storage = {};
+  someInstance.storage = {};
   extend(someInstance, queueMethods);
 
   return someInstance;
@@ -15,26 +15,23 @@ var Queue = function() {
 
 var queueMethods = {
   enqueue : function(value) {
-    var keys = Object.keys(this.storage);
-    this.storage[keys.length + 1] = value;
+    this.storage[this.size() + 1] = value;
     return this.storage;
   },
 
   dequeue : function() {
-    var keys = Object.keys(this.storage);
-    var dequeued = this.storage[keys[0]];
+    var dequeued = this.storage['1'];
 
-    delete this.storage[keys[0]];
+    delete this.storage['1'];
 
     for(var key in this.storage){
       this.storage[Number(key) - 1] = this.storage[key];
     }
 
-    delete this.storage[keys[keys.length-1]];
+    delete this.storage[keys[this.size() - 1]];
     return dequeued;
   },
   size: function(){
-    debugger;
     return Object.keys(this.storage).length;
   }
 };
