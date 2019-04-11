126a0fdc879b2d5c4e6b79f3506fe192c4aed2c8
rafeautie <rautie12@gmail.com>
2019-04-10 22:08:58 -0700

    Completed queue using ES6 syntax.

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,25 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+  }
+
+  enqueue(value) {
+    this.storage[this.size() + 1] = value;
+  }
+
+  dequeue() {
+    var dequeued = this.storage['1'];
+    delete this.storage['1'];
+    for(var key in this.storage){
+      this.storage[Number(key) - 1] = this.storage[key];
+    }
+    delete this.storage[this.size()];
+    return dequeued;
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
   }
 
 }
