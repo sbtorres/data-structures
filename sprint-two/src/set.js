578a6983e6732b8e3e2ebedb0795b9fa40f798d0
sbtorres <scott.torres27@gmail.com>
2019-04-12 15:47:25 -0700

    set.js passes all tests including new ones written.

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,36 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set.storage = {}; 
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if(this.contains(item) === false) {
+    this.storage[item] = item;
+  }
 };
 
 setPrototype.contains = function(item) {
+  for(var key of Object.keys(this.storage)){
+    if(key === item){
+      return true;
+    }
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  for(var key of Object.keys(this.storage)) {
+    if(key === item) {
+      delete this.storage[key];
+    }
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+
