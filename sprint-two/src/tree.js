355af27a26cf82dacdc957714ff1472fcf88a3d5
sbtorres <scott.torres27@gmail.com>
2019-04-11 20:29:07 -0700

    tree.js passes all tests run by the specRunner.

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,15 @@
+var extend = function (to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
+};
+
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
 
-  // your code here
-  newTree.children = null;  // fix me
+  extend(newTree, treeMethods);
+  newTree.children = [];
 
   return newTree;
 };
@@ -11,12 +17,27 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var treeInstance = Tree(value);
+  this.children.push(treeInstance);
 };
 
 treeMethods.contains = function(target) {
+  if (this.value === target) {
+    return true;
+  }
+  if (this.children.length > 0) {
+    for (let i = 0; i < this.children.length; i++) {
+      if (this.children[i].contains(target)) {
+        return true;
+      }
+    }
+  }
+  return false;
 };
 
-
+var tree = Tree();
+tree.addChild(5);
+tree.contains(5);
 
 /*
  * Complexity: What is the time complexity of the above functions?
