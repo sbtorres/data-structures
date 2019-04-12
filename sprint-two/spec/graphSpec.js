a4054db20a3e0a3d007a89dcf08b158d58cba5ef
sbtorres <scott.torres27@gmail.com>
2019-04-12 15:57:42 -0700

    Updated test file to conform to HR style guide.

--- sprint-two/spec/graphSpec.js
+++ sprint-two/spec/graphSpec.js
@@ -74,7 +74,7 @@ describe('graph', function() {
     expect(graph.hasEdge(5, 5)).to.equal(true);
   });
 
-  it('should create an instance that has an edges property that is an empty array when adding a new node', function() {
+  it ('should create an instance that has an edges property that is an empty array when adding a new node', function() {
     graph.addNode(1);
     expect(graph['1'].edges).to.eql([]);
     expect(graph['1']).to.eql({edges: []});
