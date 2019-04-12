6dd80efeb61ef90db4ddb7c07bd18a575e315ccc
sbtorres <scott.torres27@gmail.com>
2019-04-12 11:19:10 -0700

    Updated removeEdge, hasEdge, removeNode, and addEdge methods to pass some tests. Still failing certain tests.

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -19,18 +19,57 @@ Graph.prototype.contains = function(node) {
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  // check if this[node].edges length is > 0
+  if(this[node].edges.length > 0){
+    // if so, use forEachNode on this[node].edges to perform this[node].removeEdge(node, fromNode[i])
+    this.forEachNode(function(node2){
+      for(let i = 0; i < node2.edges.length; i++){
+        this.removeEdge(node, node2.edges[i]);
+      }
+    });
+  }    
+  // delete this[node]
+  delete this[node];
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  //check if this[fromNode].edges contains toNode
+  return this[fromNode].edges.includes(toNode);
+
+    //check if this[toNode].edges contains fromNode
+      // return true
+  // return false
+
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  // if does not have edge
+  if(!this[fromNode].edges.includes(toNode)){
+    // push 'toNode' to this[fromNode].edges
+    // push 'fromNode' to this[toNode].edges
+    this[fromNode].edges.push(toNode);
+    this[toNode].edges.push(fromNode);
+  }
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  // loop through this[fromNode].edges to find 'toNode'
+  for(let i = 0; i < this[fromNode].edges.length; i++){
+    // if 'toNode' is found, splice(this[fromNode].edges.indexOf(toNode), 1);
+    if(this[fromNode].edges[i] === toNode){
+      this[fromNode].edges.splice(i, 1);
+    }
+  }
+    // loop through this[toNode].edges to find 'fromNode'
+  for(let i = 0; i < this[toNode].edges.length; i++){
+    // if 'fromNode' is found, splice(this[toNode].edges.indexOf(fromNode), 1);
+    if(this[toNode].edges[i] === fromNode){
+      this[toNode].edges.splice(i, 1);
+    }
+  } 
 };
 
 // Pass in a callback which will be executed on each node of the graph.
