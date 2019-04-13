0007f96bb9ea8fe9ac7202636b71ad85f92efc07
sbtorres <scott.torres27@gmail.com>
2019-04-13 12:14:21 -0700

    Revert "binarySearchTree.js passes all given tests plus additional user defined tests."
    This reverts commit 944dc1d41a98cc0cdaf93785dd52c004712fa20f.

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,66 +1,7 @@
 var BinarySearchTree = function(value) {
-  this.value = value;
-  this.left = undefined;
-  this.right = undefined;
-};
-
-BinarySearchTree.prototype.insert = function(value) {
-  var search = function(node) {
-    if (value === node.value) {
-      return;
-    } else if (value < node.value) {
-      if (node.left === undefined) {
-        node.left = new BinarySearchTree(value);
-      } else {
-        search(node.left);
-      }
-    } else {
-      if (node.right === undefined) {
-        node.right = new BinarySearchTree(value);
-      } else {
-        search(node.right);
-      }
-    }
-  };
-  search(this);
-};
-
-BinarySearchTree.prototype.contains = function(value) {
-
-  var search = function(node) {
-    if (value === node.value) {
-      return true;
-    }
-    if (value < node.value) {
-      if (node.left) {
-        return search(node.left);
-      }
-    }
-    if (value > node.value) {
-      if (node.right !== undefined) {
-        return search(node.right);
-      }
-    }
-    return false;
-  };
-  return search(this);
-};
-
-BinarySearchTree.prototype.depthFirstLog = function(cb) {
-  cb(this.value);
-  if (!this.left && !this.right) {
-    return;
-  }
-  if (this.left) {
-    this.depthFirstLog.call(this.left, cb);
-  }
-  if (this.right) {
-    this.depthFirstLog.call(this.right, cb);
-  }
 };
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
